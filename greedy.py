702534078e07e53974b34a91dba568a0c5d0f34d
Diego Gonzalez Cina <gonzalezcina.diego@gmail.com>
2024-10-11 20:46:11 -0300

    Fixed Greedy tests

--- greedy.py
+++ greedy.py
@@ -1,15 +1,12 @@
-def obtener_moneda(a, b, l, r, turno):
-    if turno:
+def obtener_moneda_mas_grande(a, b, l, r):
         if a > b:
-            l+=1
-            return a , l , r
-        r-=1
-        return b, l , r
+            return a , l+1 , r
+        return b, l , r-1
+
+def obtener_moneda_mas_chica(a, b, l, r):
     if a < b:
-        l+=1
-        return a , l , r
-    r-=1
-    return b, l , r
+        return a , l+1 , r
+    return b, l , r-1
 
 def main(monedas):
     contador_Sophia = 0
@@ -19,11 +16,11 @@ def main(monedas):
     turno_sophi = True
     while (l<=r):
         if turno_sophi:    
-            m, l, r = obtener_moneda(monedas[l], monedas[r], l, r, turno_sophi)
+            m, l, r = obtener_moneda_mas_grande(monedas[l], monedas[r], l, r)
             contador_Sophia += m
             turno_sophi = False
             continue
-        m, l, r = obtener_moneda(monedas[l], monedas[r], l, r, turno_sophi)
+        m, l, r = obtener_moneda_mas_chica(monedas[l], monedas[r], l, r)
         contador_Mateo += m
         turno_sophi = True
 
