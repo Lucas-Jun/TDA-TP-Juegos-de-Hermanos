6b032d673f7545f8c9facd909d24e7c8d7bbac1a
IYuschak <ignacioyuschak11@gmail.com>
2024-11-10 20:22:29 -0300

    Test_Greedy

--- test_greedy.py
+++ test_greedy.py
@@ -49,6 +49,32 @@ class TestGreegy(unittest.TestCase):
         self.lista = set_files.set_coins(new_path)      
         res_s, _, _ = greedy.solucion_greedy(self.lista)
         self.assertEqual(res_s, 7139357)
+
+    def test_Bordes_Iguales(self):
+        new_path = self.cur_path + '\\test-juegoDeHermanos\\parte1-tests\\Bordes-Iguales.txt'
+        self.lista = set_files.set_coins(new_path)      
+        res_s, _, _ = greedy.solucion_greedy(self.lista)
+        self.assertEqual(res_s, 153)
+
+    def test_Cantidad_Impar(self):
+        new_path = self.cur_path + '\\test-juegoDeHermanos\\parte1-tests\\Cantidad-Impar.txt'
+        self.lista = set_files.set_coins(new_path)      
+        res_s, _, _ = greedy.solucion_greedy(self.lista)
+        self.assertEqual(res_s, 32)
+    
+    def test_Orden_Creciente(self):
+        new_path = self.cur_path + '\\test-juegoDeHermanos\\parte1-tests\\Orden-Creciente.txt'
+        self.lista = set_files.set_coins(new_path)      
+        res_s, _, _ = greedy.solucion_greedy(self.lista)
+        self.assertEqual(res_s, 51)
+    
+
+    def test_Orden_Decreciente(self):
+        new_path = self.cur_path + '\\test-juegoDeHermanos\\parte1-tests\\Orden-Decreciente.txt'
+        self.lista = set_files.set_coins(new_path)      
+        res_s, _, _ = greedy.solucion_greedy(self.lista)
+        self.assertEqual(res_s, 51)
+
     
 
 if __name__ == '__main__':
